a3635bdaa85411f4a2fcc43ce31d40cfe168caec
sstoveri <sstoveri45@gmail.com>
2022-02-21 11:56:31 -0600

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlayingWithVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable Declared and initalized a variable.
+            int dogAge = 5;
+            string dogName = "Reef";
+            bool likesHiking = true;
+            char nickName = 'R';
+            double bones = 2;
+            decimal weight = 50.3m;
+
+            //string interpolation with the $ and { } around variables
+            Console.WriteLine($"My dog's name is {dogName}. She is {dogAge} years old and she has {bones} bones and it is {likesHiking} that she loves to hike, which keeps her at her weight of {weight}");
+
+
+        }
+    }
+}
